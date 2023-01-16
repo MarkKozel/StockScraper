57cb3dbd523661e9851944a1b100cc1b7e593795
Mark Kozel <markkozel+dev@fastmail.com>
2023-01-15 20:44:51 -0800

    Working w/ noon, wed, csv

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
 # StockScraper
+
+Grabs price and volume for each stock at noon on Wednesdays using the TIME_SERIES_INTRADAY API option using alphavantage.co free service
+
+Outputs csv files in the following format:
+```cvs
+date, price, volume, INTC as of 2023-01-13 20:00:00, Data from Noon Wednesdays US/Eastern
+1-11-2023, 29.2900, 3326247
+1-4-2023, 27.7750, 4031388
+12-28-2022, 25.5901, 2881791
+12-21-2022, 26.8400, 3269791
+12-14-2022, 28.8650, 2255008
+12-7-2022, 28.3400, 3190003
+11-30-2022, 28.4650, 3190562
+11-23-2022, 29.6240, 2775404
+```
+
+## Configuration
+
+Create a file names ```config.js```
+Add the following elements:
+```js
+module.exports = {
+  apikey: "your-api-key-here",
+  tickers: [
+    "TICR1",
+    "TICR2"
+    ...
+  ]
+}
+```
+
+> The free ```alphavantage.co``` api key only allows 5 reads per minute...or something like that
--- index.js
+++ index.js
@@ -1,22 +1,63 @@
 'use strict'
 var request = require('request');
 const config = require('./config');
+const fs = require('fs');
 
-console.log(`API Key is ${config.apikey}`);
+const WeekdayOfInterest = 3; //Wed
+const series = `TIME_SERIES_INTRADAY`;
+const sampleInterval = '60min';
+const outputSize = 'full';
 
-let url = `https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=${config.apikey}`;
+config.tickers.forEach(key => {
+  getData(key);
+})
 
-request.get({
-  url: url,
-  json: true,
-  headers: { 'User-Agent': 'request' }
-}, (err, res, data) => {
-  if (err) {
-    console.log('Error:', err);
-  } else if (res.statusCode !== 200) {
-    console.log('Status:', res.statusCode);
-  } else {
-    // data is successfully parsed as a JSON object:
-    console.log(data['Meta Data']);
-  }
-});
+function getData(stockSymbol) {
+  const fileName = `./${stockSymbol}.csv`;
+  console.info(`Getting data for ${stockSymbol}:`);
+
+  let url = `https://www.alphavantage.co/query?function=${series}&symbol=${stockSymbol}&interval=${sampleInterval}&outputsize=${outputSize}&datatype=json&apikey=${config.apikey}`;
+  console.log(`Request: ${url}`);
+
+  request.get({
+    url: url,
+    json: true,
+    headers: { 'User-Agent': 'request' }
+  }, (err, res, data) => {
+    if (err) {
+      console.log('Error:', err);
+    } else if (res.statusCode !== 200) {
+      console.log('Status:', res.statusCode);
+    } else {
+      // data is successfully parsed as a JSON object:
+      const meta = data['Meta Data'];
+      const asOfDate = meta[`3. Last Refreshed`];
+      const tZone = meta[`6. Time Zone`];
+
+      //Write Header to file
+      try {
+        fs.writeFileSync(fileName, `date, price, volume, ${stockSymbol} as of ${asOfDate}, Data from Noon Wednesdays ${tZone}`);
+      } catch (err) {
+        console.error(err);
+      }
+
+      //Write date elements to file
+      const hourly = data['Time Series (60min)'];
+      for (const key in hourly) {
+        const theDate = new Date(key);
+        // const keyArray = key.split(' ');
+        // console.info(keyArray[0], keyArray[1]);
+        if ((theDate.getHours() == 12) && (theDate.getDay() === WeekdayOfInterest)) {
+          const stockValues = hourly[key];
+          console.info(theDate.toString(), stockValues[`4. close`], stockValues[`5. volume`]);
+          try {
+            const dataStr = `${theDate.getMonth() + 1}-${theDate.getDate()}-${theDate.getFullYear()}`;
+            fs.appendFileSync(fileName, `\n${dataStr}, ${stockValues[`4. close`]}, ${stockValues[`5. volume`]}`);
+          } catch (err) {
+            console.error(err);
+          }
+        }
+      }
+    }
+  });
+}
